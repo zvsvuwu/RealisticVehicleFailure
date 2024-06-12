c542bc5a5b2e310f66b34d51c7179658e969d997
zvsv <98418284+zvsvuwu@users.noreply.github.com>
2024-06-12 15:34:17 +0200

    Add files via upload

---
+++ RealisticVehicleFailure/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "bodacious"
+
+games {"gta5"}
+lua54 'yes'
+
+client_scripts {
+	"config.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"config.lua",
+	"server.lua"
+}
---
+++ RealisticVehicleFailure/server.lua
@@ -0,0 +1,9 @@
+local function checkWhitelist(id)
+	for key, value in pairs(RepairWhitelist) do
+		if id == value then
+			return true
+		end
+	end	
+	return false
+end
+
